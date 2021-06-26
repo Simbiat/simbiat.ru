397b0becde31ebaac6dc5a9a8708546180b294c7
Simbiat <simbiat@outlook.com>
2021-06-26 10:23:19 +0300

    Clean-up for new design

--- js/!new/Webshare.js
+++
@@ -1,9 +0,0 @@
-//WebShare API call
-function webShare() {
-    'use strict';
-    navigator.share({
-        title: document.title,
-        text: getMeta('og:description') ?? getMeta('description'),
-        url: document.location,
-    });
-}
