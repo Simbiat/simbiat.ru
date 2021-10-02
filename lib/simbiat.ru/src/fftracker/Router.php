c55dce390fdd9115bbddec0f77a8b9673b073ccb
Simbiat <simbiat@outlook.com>
2021-10-02 12:49:39 +0300

    Preparation for fftracker abstractions

---
+++ lib/simbiat.ru/src/fftracker/Router.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+namespace Simbiat\fftracker;
+
+class Router extends \Simbiat\Abstracts\Router
+{
+    #List supported "paths". Basic ones only, some extra validation may be required further
+    protected array $subRoutes = ['search'];
+    #Current breadcrumb for navigation
+    protected array $breadCrumb = [
+        ['href'=>'/fftracker/', 'name'=>'FFXIV Tracker']
+    ];
+    protected string $title = 'Final Fantasy XIV Tracker';
+    protected string $h1 = 'Final Fantasy XIV Tracker';
+    protected string $ogdesc = 'Tracker for Final Fantasy XIV entities and respective statistics';
+    protected string $serviceName = 'fftracker';
+
+    #This is actual page generation based on further details of the $path
+    protected function pageGen(array $path): array
+    {
+        return match(strtolower($path[0])) {
+            'search' => [],
+        };
+    }
+}
