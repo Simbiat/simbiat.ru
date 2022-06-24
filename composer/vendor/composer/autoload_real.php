9fd70a20ea744534a6422b88ef7af3627a164fd8
Dmitry Kustov <simbiat@outlook.com>
2022-06-24 17:51:16 +0300

    Composer and libraries update

--- composer/vendor/composer/autoload_real.php
+++ composer/vendor/composer/autoload_real.php
@@ -25,29 +25,16 @@ class ComposerAutoloaderInit2f59fc41db9a7c853d25dccb9ce2a2e1
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInit2f59fc41db9a7c853d25dccb9ce2a2e1', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit2f59fc41db9a7c853d25dccb9ce2a2e1', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
-
-            call_user_func(\Composer\Autoload\ComposerStaticInit2f59fc41db9a7c853d25dccb9ce2a2e1::getInitializer($loader));
-        } else {
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
-        }
+        require __DIR__ . '/autoload_static.php';
+        call_user_func(\Composer\Autoload\ComposerStaticInit2f59fc41db9a7c853d25dccb9ce2a2e1::getInitializer($loader));
 
         $loader->setClassMapAuthoritative(true);
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInit2f59fc41db9a7c853d25dccb9ce2a2e1::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
+        $includeFiles = \Composer\Autoload\ComposerStaticInit2f59fc41db9a7c853d25dccb9ce2a2e1::$files;
         foreach ($includeFiles as $fileIdentifier => $file) {
             composerRequire2f59fc41db9a7c853d25dccb9ce2a2e1($fileIdentifier, $file);
         }
