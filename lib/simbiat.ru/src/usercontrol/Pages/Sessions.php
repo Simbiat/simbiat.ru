2a432f1ba2eb663349cd0a55a5ac1d44a4b72b2d
Dmitry Kustov <simbiat@outlook.com>
2022-08-25 07:56:37 +0300

    Basic logic and layout for sessions' page

--- lib/simbiat.ru/src/usercontrol/Pages/Sessions.php
+++ lib/simbiat.ru/src/usercontrol/Pages/Sessions.php
@@ -2,7 +2,11 @@
 declare(strict_types=1);
 namespace Simbiat\usercontrol\Pages;
 
+use DeviceDetector\DeviceDetector;
+use DeviceDetector\Parser\AbstractParser;
+use DeviceDetector\Parser\Device\AbstractDeviceParser;
 use Simbiat\Abstracts\Page;
+use Simbiat\Config\Common;
 use Simbiat\HomePage;
 
 class Sessions extends Page
@@ -30,6 +34,39 @@ class Sessions extends Page
     protected function generate(array $path): array
     {
         $outputArray = [];
+        #Get sessions
+        $outputArray['sessions'] = HomePage::$dbController->selectAll('SELECT `time`, `cookieid`, `sessionid`, `uc__sessions`.`ip`, `country`, `city`, `useragent` FROM `uc__sessions` LEFT JOIN `seo__ips` ON `seo__ips`.`ip`=`uc__sessions`.`ip` WHERE `userid`=:userid ORDER BY `time` DESC', [':userid' => $_SESSION['userid']]);
+        #Get cookies
+        $outputArray['cookies'] = HomePage::$dbController->selectAll('SELECT `time`, `cookieid`, `uc__cookies`.`ip`, `country`, `city`, `useragent` FROM `uc__cookies` LEFT JOIN `seo__ips` ON `seo__ips`.`ip`=`uc__cookies`.`ip` WHERE `userid`=:userid ORDER BY `time` DESC', [':userid' => $_SESSION['userid']]);
+        #Get logs
+        $outputArray['logs'] = HomePage::$dbController->selectAll('SELECT `time`, `action`, `sys__logs`.`ip`, `country`, `city`, `useragent` FROM `sys__logs` LEFT JOIN `seo__ips` ON `seo__ips`.`ip`=`sys__logs`.`ip` WHERE `userid`=:userid AND `type` IN (1, 2, 8) ORDER BY `time` DESC LIMIT 50', [':userid' => $_SESSION['userid']]);
+        #Create useragent object
+        #Force full versions
+        AbstractDeviceParser::setVersionTruncation(AbstractParser::VERSION_TRUNCATION_NONE);
+        #Initialize device detector
+        $dd = (new DeviceDetector());
+        #Expand useragent
+        foreach (['sessions', 'cookies', 'logs'] as $type) {
+            foreach ($outputArray[$type] as $key => $item) {
+                $dd->setUserAgent($item['useragent']);
+                $dd->parse();
+                #Get OS
+                $outputArray[$type][$key]['os'] = $dd->getOs();
+                #Get client
+                $outputArray[$type][$key]['client'] = $dd->getClient();
+                #Set OS and client icon if they exist
+                if (is_file(Common::$imgDir.'/devicedetector/os/'.$outputArray[$type][$key]['os']['name'].'.webp')) {
+                    $outputArray[$type][$key]['os']['icon'] = '/img/devicedetector/os/'.$outputArray[$type][$key]['os']['name'].'.webp';
+                }
+                if (is_file(Common::$imgDir.'/devicedetector/'.$outputArray[$type][$key]['client']['type'].'/'.$outputArray[$type][$key]['client']['name'].'.webp')) {
+                    $outputArray[$type][$key]['client']['icon'] = '/img/devicedetector/'.$outputArray[$type][$key]['client']['type'].'/'.$outputArray[$type][$key]['client']['name'].'.webp';
+                }
+                #Set country icon, if flag exists
+                if (!empty($item['country']) && is_file(Common::$imgDir.'/flags/'.$item['country'].'.svg')) {
+                    $outputArray[$type][$key]['countryIcon'] = '/img/flags/'.$item['country'].'.svg';
+                }
+            }
+        }
         return $outputArray;
     }
 }
