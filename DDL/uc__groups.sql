a6edb186d86fadf3394a8b19c21dc88c1100e4cf
Dmitry Kustov <simbiat@outlook.com>
2022-11-24 13:29:01 +0200

    Added description to groups

--- DDL/uc__groups.sql
+++ DDL/uc__groups.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `uc__groups`
 (
-    `groupid`   INT UNSIGNED AUTO_INCREMENT COMMENT 'ID of the group' PRIMARY KEY,
-    `groupname` VARCHAR(25) NOT NULL COMMENT 'Human-ready group name',
+    `groupid`     INT UNSIGNED AUTO_INCREMENT COMMENT 'ID of the group' PRIMARY KEY,
+    `groupname`   VARCHAR(25) NOT NULL COMMENT 'Human-ready group name',
+    `description` TEXT        NOT NULL COMMENT 'Description of the group',
     CONSTRAINT `groupname` UNIQUE (`groupname`)
 );
