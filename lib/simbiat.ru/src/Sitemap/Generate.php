3859a0b278d9c85d1191b2b20f794ddef7f57eaa
Dmitry Kustov <simbiat@outlook.com>
2023-01-16 18:01:09 +0200

    Notify Google about updates for each sitemap specifically

--- lib/simbiat.ru/src/Sitemap/Generate.php
+++ lib/simbiat.ru/src/Sitemap/Generate.php
@@ -14,11 +14,9 @@ class Generate
     public function generate(): bool
     {
         try {
-            #Remove old files
+            #Remove old text files
             array_map('unlink', glob( Common::$sitemap.'txt/*.txt'));
             array_map('unlink', glob( Common::$sitemap.'txt/*/*.txt'));
-            array_map('unlink', glob( Common::$sitemap.'xml/*.xml'));
-            array_map('unlink', glob( Common::$sitemap.'xml/*/*.xml'));
             #Set method, since router requires it
             HomePage::$method = 'GET';
             #Cache router
@@ -29,10 +27,17 @@ class Generate
             #Write text file
             file_put_contents(Common::$sitemap.'txt/index.txt', $index);
             $links = explode("\n", $index);
+            $xmlFiles = array_merge(glob( Common::$sitemap.'xml/*.xml'), glob( Common::$sitemap.'xml/*/*.xml'));
+            #Remove XML files, which are no longer exist as per new index
+            foreach ($xmlFiles as $file) {
+                if (preg_match('/^.*xml\/index\.xml$/ui', $file) === 0 && !in_array(Common::$baseUrl.'/sitemap/'.str_replace(Common::$sitemap, '', str_replace('xml', 'txt', $file)), $links)) {
+                    @unlink($file);
+                }
+            }
             #Write XML index
             $twigVars = $router->route(['xml']);
             $index = Twig::getTwig()->render($twigVars['template_override'] ?? 'index.twig', $twigVars);
-            file_put_contents(Common::$sitemap.'xml/index.xml', $index);
+            $curl = (new Curl);
             #Generate sitemaps for each link in index
             foreach ($links as $link) {
                 if (!empty($link)) {
@@ -48,15 +53,35 @@ class Generate
                         }
                         #Generate and write file
                         $mapVars = $router->route(array_merge([$format], $path));
-                        file_put_contents($filePath.'/'.$fileName, Twig::getTwig()->render($mapVars['template_override'] ?? 'index.twig', $mapVars));
+                        $content = Twig::getTwig()->render($mapVars['template_override'] ?? 'index.twig', $mapVars);
+                        if ($format === 'xml') {
+                            #Check if file already exists and if its contents are the same
+                            if (!is_file($filePath.'/'.$fileName) || $index === file_get_contents($filePath.'/'.$fileName)) {
+                                #Save the file
+                                file_put_contents($filePath.'/'.$fileName, $content);
+                                #Ping Google about file update
+                                try {
+                                    $curl->getPage('https://www.google.com/ping?sitemap='.urlencode(str_replace('txt', 'xml', $link)));
+                                } catch (\Throwable) {
+                                    #Do nothing, it's not critical
+                                }
+                            }
+                        } else {
+                            #For text, we just save the file
+                            file_put_contents($filePath.'/'.$fileName, $content);
+                        }
                     }
                 }
             }
-            #Ping Google about update
-            try {
-                (new Curl)->getPage('https://www.google.com/ping?sitemap='.urlencode(Common::$baseUrl.'/sitemap/xml/index.xml'));
-            } catch (\Throwable) {
-                #Do nothing, it's not critical
+            #Check if index file already exists and if its contents are the same
+            if (!is_file(Common::$sitemap.'xml/index.xml') || $index === file_get_contents(Common::$sitemap.'xml/index.xml')) {
+                file_put_contents(Common::$sitemap.'xml/index.xml', $index);
+                #Ping Google about index update
+                try {
+                    $curl->getPage('https://www.google.com/ping?sitemap='.urlencode(Common::$baseUrl.'/sitemap.xml'));
+                } catch (\Throwable) {
+                    #Do nothing, it's not critical
+                }
             }
             return true;
         } catch (\Throwable $exception) {
