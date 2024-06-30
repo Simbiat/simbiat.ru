04f9f2984fa1a4b5dc8b623109e1c2fa71bb6482
Dmitry Kustov <simbiat@outlook.com>
2024-06-30 15:29:08 +0300

    New icons

--- public/index.php
+++ public/index.php
@@ -6,16 +6,16 @@ declare(strict_types = 1);
 #Load composer libraries
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
-use Simbiat\Config;
-use Simbiat\HomePage;
+use Simbiat\Website\Config;
+use Simbiat\Website\HomePage;
 
 #Generate basic settings
 new Config();
 
 #Set error handling
-set_error_handler('\Simbiat\Errors::error_handler');
-set_exception_handler('\Simbiat\Errors::error_log');
-register_shutdown_function('\Simbiat\Errors::shutdown');
+set_error_handler('\Simbiat\Website\Errors::error_handler');
+set_exception_handler('\Simbiat\Website\Errors::error_log');
+register_shutdown_function('\Simbiat\Website\Errors::shutdown');
 
 $HomePage = new HomePage();
 exit;
