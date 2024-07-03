8f624aa392016f5c722cb94f8ec5dc31969a64b2
Dmitry Kustov <simbiat@outlook.com>
2024-07-03 15:58:13 +0300

    Support for more errors, use webp instead of gif

--- lib/Website/Routing/MainRouter.php
+++ lib/Website/Routing/MainRouter.php
@@ -81,7 +81,7 @@ class MainRouter extends \Simbiat\Website\Abstracts\Router
 
     #Function to help route error pages on frontend
     private function error(array $uri): array {
-        if (empty($uri[0]) || !in_array((int)$uri[0], [300, 301, 302, 303, 305, 307, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 500, 501, 502, 503, 504, 505])) {
+        if (empty($uri[0]) || !in_array((int)$uri[0], [300, 301, 302, 303, 305, 307, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 421, 422, 423, 424, 425, 426, 428, 429, 431, 451, 500, 501, 502, 503, 504, 505])) {
             $outputArray['http_error'] = 404;
         } else {
             $outputArray['http_error'] = (int)$uri[0];
