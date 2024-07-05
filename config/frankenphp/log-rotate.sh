4acaa7f3f8f7e8ae353ff307cbeb692ebfa0b662
Dmitry Kustov <simbiat@outlook.com>
2024-07-05 17:06:38 +0300

    Log rotation

---
+++ config/frankenphp/log-rotate.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#Source environments (required for cron)
+. /usr/local/php/config/caddy.env
+. /usr/local/php/config/php.env
+
+if [ -f /app/logs/caddy.log ]; then
+    # Copy the log file to the new file with the previous day's date
+    cp /app/logs/caddy.log "/app/logs/caddy-$(date -d "yesterday" +%Y.%m.%d).log"
+    # Truncate the original log file
+    truncate -s 0 /app/logs/caddy.log
+fi
+if [ -f /app/logs/php.log ]; then
+    # Copy the log file to the new file with the previous day's date
+    cp /app/logs/php.log "/app/logs/php-$(date -d "yesterday" +%Y.%m.%d).log"
+    # Truncate the original log file
+    truncate -s 0 /app/logs/php.log
+fi
+exit
---
+++ config/mysql/log-rotate.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#Source environments (required for cron)
+. /etc/mysql/conf.d/mariadb.env
+
+if [ -f /app/logs/mariadb.log ]; then
+    # Copy the log file to the new file with the previous day's date
+    cp /app/logs/mariadb.log "/app/logs/mariadb-$(date -d "yesterday" +%Y.%m.%d).log"
+    # Truncate the original log file
+    truncate -s 0 /app/logs/mariadb.log
+fi
+exit
