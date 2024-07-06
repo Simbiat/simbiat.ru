9b670bef9ae3d3933d7cf27b6d0d7054d4756b7e
Dmitry Kustov <simbiat@outlook.com>
2024-07-06 08:24:56 +0300

    Corrected path

--- config/mysql/log-rotate.sh
+++ config/mysql/log-rotate.sh
@@ -3,10 +3,10 @@
 #Source environments (required for cron)
 . /etc/mysql/conf.d/mariadb.env
 
-if [ -f /app/logs/mariadb.log ]; then
+if [ -f /usr/local/logs/mariadb.log ]; then
     # Copy the log file to the new file with the previous day's date
-    cp -p /app/logs/mariadb.log "/app/logs/mariadb-$(date -d "yesterday" +%Y.%m.%d).log"
+    cp -p /usr/local/logs/mariadb.log "/usr/local/logs/mariadb-$(date -d "yesterday" +%Y.%m.%d).log"
     # Truncate the original log file
-    truncate -s 0 /app/logs/mariadb.log
+    truncate -s 0 /usr/local/logs/mariadb.log
 fi
 exit
