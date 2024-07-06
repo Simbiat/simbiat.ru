6f984934618111c7c251cec3fd36488b40aef99c
Dmitry Kustov <simbiat@outlook.com>
2024-07-06 08:22:19 +0300

    Preserve timestamps when rotating logs

--- config/frankenphp/log-rotate.sh
+++ config/frankenphp/log-rotate.sh
@@ -6,13 +6,13 @@
 
 if [ -f /app/logs/caddy.log ]; then
     # Copy the log file to the new file with the previous day's date
-    cp /app/logs/caddy.log "/app/logs/caddy-$(date -d "yesterday" +%Y.%m.%d).log"
+    cp -p /app/logs/caddy.log "/app/logs/caddy-$(date -d "yesterday" +%Y.%m.%d).log"
     # Truncate the original log file
     truncate -s 0 /app/logs/caddy.log
 fi
 if [ -f /app/logs/php.log ]; then
     # Copy the log file to the new file with the previous day's date
-    cp /app/logs/php.log "/app/logs/php-$(date -d "yesterday" +%Y.%m.%d).log"
+    cp -p /app/logs/php.log "/app/logs/php-$(date -d "yesterday" +%Y.%m.%d).log"
     # Truncate the original log file
     truncate -s 0 /app/logs/php.log
 fi
--- config/mysql/log-rotate.sh
+++ config/mysql/log-rotate.sh
@@ -5,7 +5,7 @@
 
 if [ -f /app/logs/mariadb.log ]; then
     # Copy the log file to the new file with the previous day's date
-    cp /app/logs/mariadb.log "/app/logs/mariadb-$(date -d "yesterday" +%Y.%m.%d).log"
+    cp -p /app/logs/mariadb.log "/app/logs/mariadb-$(date -d "yesterday" +%Y.%m.%d).log"
     # Truncate the original log file
     truncate -s 0 /app/logs/mariadb.log
 fi
