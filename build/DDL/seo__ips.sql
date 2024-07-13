e034ac0b66fb022298e9c51b1957382f9e2058fc
Dmitry Kustov <simbiat@outlook.com>
2024-07-13 17:58:57 +0300

    Minor Argon adjustments

--- build/DDL/seo__ips.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `seo__ips` (
-  `ip` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_nopad_as_ci NOT NULL COMMENT 'IP address',
-  `country` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_nopad_ai_ci NOT NULL COMMENT 'Country name',
-  `city` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_nopad_ai_ci NOT NULL COMMENT 'City name',
-  PRIMARY KEY (`ip`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_nopad_as_cs COMMENT='IP to country and city based on ipinfo.io' `PAGE_COMPRESSED`='ON' ROW_FORMAT=Dynamic;
