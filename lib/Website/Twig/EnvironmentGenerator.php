2041a06c01e80c8fd8faf483186e914837ea89cd
Dmitry Kustov <simbiat@outlook.com>
2024-07-14 15:08:41 +0300

    Renamed Twig folder

--- lib/Website/Twig/EnvironmentGenerator.php
+++ lib/Website/Twig/EnvironmentGenerator.php
@@ -5,18 +5,24 @@ namespace Simbiat\Website\Twig;
 
 #Twig environment
 use Simbiat\Website\Config;
-use Simbiat\Website\Twig\Extension;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
+/**
+ * Class to prepare Twig environment object
+ */
 final class EnvironmentGenerator
 {
     private static ?Environment $environment = null;
     
+    /**
+     * Function to get the actual Twig environment object
+     * @return \Twig\Environment
+     */
     public static function getTwig(): Environment
     {
         if (!self::$environment) {
-            $templatesDir = Config::$workDir.'/twig/';
+            $templatesDir = Config::$workDir.'/templates/';
             #Initiate Twig
             self::$environment = new Environment(new FilesystemLoader($templatesDir), ['cache' => Config::$workDir.'/data/cache/twig/', 'auto_reload' => true, 'autoescape' => 'html', 'use_yield' => true]);
             self::$environment->addExtension(new Extension());
