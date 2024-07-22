e275f287c9612447ddef1d26f362cc45e4f57a97
Dmitry Kustov <simbiat@outlook.com>
2024-07-22 07:25:53 +0300

    Fulltext index for old names

--- build/DDL/ffxiv__character_names.sql
+++ build/DDL/ffxiv__character_names.sql
@@ -2,5 +2,6 @@ CREATE TABLE `ffxiv__character_names` (
   `characterid` int(10) unsigned NOT NULL COMMENT 'Character ID taken from Lodestone URL (https://eu.finalfantasyxiv.com/lodestone/character/characterid/)',
   `name` varchar(50) NOT NULL COMMENT 'Character''s previous name',
   PRIMARY KEY (`characterid`,`name`),
+  FULLTEXT KEY `name` (`name`),
   CONSTRAINT `char_names_id` FOREIGN KEY (`characterid`) REFERENCES `ffxiv__character` (`characterid`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_nopad_as_cs ROW_FORMAT=DYNAMIC COMMENT='Past names used by characters' `PAGE_COMPRESSED`='ON';
--- build/DDL/ffxiv__freecompany_names.sql
+++ build/DDL/ffxiv__freecompany_names.sql
@@ -2,5 +2,6 @@ CREATE TABLE `ffxiv__freecompany_names` (
   `freecompanyid` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_nopad_as_ci NOT NULL COMMENT 'Free Company ID taken from Lodestone URL (https://eu.finalfantasyxiv.com/lodestone/freecompany/freecompanyid/)',
   `name` varchar(50) NOT NULL COMMENT 'Previous name of the company',
   PRIMARY KEY (`freecompanyid`,`name`),
+  FULLTEXT KEY `name` (`name`),
   CONSTRAINT `fc_names_id` FOREIGN KEY (`freecompanyid`) REFERENCES `ffxiv__freecompany` (`freecompanyid`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_nopad_as_cs ROW_FORMAT=DYNAMIC COMMENT='Past names of the companies' `PAGE_COMPRESSED`='ON';
