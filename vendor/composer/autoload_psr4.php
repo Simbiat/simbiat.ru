a769f503e4770923bc62d0586a818b7c2de26d3f
Dmitry Kustov <simbiat@outlook.com>
2024-09-10 07:01:13 +0300

    Updated libraries

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -7,6 +7,7 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Twig\\' => array($vendorDir . '/twig/twig/src'),
+    'Symfony\\Polyfill\\Php81\\' => array($vendorDir . '/symfony/polyfill-php81'),
     'Symfony\\Polyfill\\Php80\\' => array($vendorDir . '/symfony/polyfill-php80'),
     'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
     'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
@@ -15,7 +16,8 @@ return array(
     'Simbiat\\Website\\' => array($baseDir . '/lib/Website'),
     'Simbiat\\Database\\' => array($baseDir . '/lib/Database/src'),
     'Simbiat\\Cron\\' => array($baseDir . '/lib/Cron/src'),
-    'Simbiat\\' => array($baseDir . '/lib/AccountKeying/src', $baseDir . '/lib/array2table/src', $baseDir . '/lib/ArrayHelpers/src', $baseDir . '/lib/CuteBytes/src', $baseDir . '/lib/HTMLCache/src', $baseDir . '/lib/HTMLCut/src', $baseDir . '/lib/Lodestone/src', $baseDir . '/lib/nl2tag/src', $baseDir . '/lib/optimizeTables/src', $baseDir . '/lib/SafeFileName/src', $baseDir . '/lib/SandClock/src'),
+    'Simbiat\\ArrayToTable\\' => array($baseDir . '/lib/array2table/src'),
+    'Simbiat\\' => array($baseDir . '/lib/AccountKeying/src', $baseDir . '/lib/ArrayHelpers/src', $baseDir . '/lib/CuteBytes/src', $baseDir . '/lib/HTMLCache/src', $baseDir . '/lib/HTMLCut/src', $baseDir . '/lib/Lodestone/src', $baseDir . '/lib/nl2tag/src', $baseDir . '/lib/optimizeTables/src', $baseDir . '/lib/SafeFileName/src', $baseDir . '/lib/SandClock/src'),
     'SendGrid\\Stats\\' => array($vendorDir . '/sendgrid/sendgrid/lib/stats'),
     'SendGrid\\Mail\\' => array($vendorDir . '/sendgrid/sendgrid/lib/mail'),
     'SendGrid\\Helper\\' => array($vendorDir . '/sendgrid/sendgrid/lib/helper'),
