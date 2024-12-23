f7b167b56a11b6d39dc2c1ef28b6d75fb0f123cf
Dmitry Kustov <simbiat@outlook.com>
2024-12-23 12:11:24 +0200

    Zip everything after backup, so that logical and physical backups are close to each other

--- config/mysql/cron/backup.sh
+++ config/mysql/cron/backup.sh
@@ -21,19 +21,19 @@ if [ "$WEB_SERVER_TEST" != "true" ]; then
     rm -rf $physBackup;
     echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] Backing up users for $currentDate...";
     mariadb-dump --all-tablespaces --opt --add-drop-database --default-character-set=utf8mb4 --flush-privileges --flush-logs --tz-utc --quote-names --single-transaction --insert-ignore --system=users 2>> "$logFile" 1>> "$logicBackup/$currentDate-users.sql"
-    echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] Zipping users for $currentDate...";
-    7z a -aoa -y -r -stl -sdel -sse -ssp -ssw -ssc -bt -m0=LZMA2 -p"${MARIADB_BACKUP_PASSWORD}" "$logicBackup/$currentDate-users.7z" "$logicBackup/$currentDate-users.sql";
     echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] Backing up data for $currentDate (logical)...";
-    mariadb-dump --all-tablespaces --opt --add-drop-database --default-character-set=utf8mb4 --flush-privileges --flush-logs --tz-utc --quote-names --single-transaction --insert-ignore --routines --triggers --databases simbiatr_simbiat --tables $tablesOrder 2>> "$logFile" 1>> "$logicBackup/$currentDate-${logicalName}.sql"
-    echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] Zipping data for $currentDate (logical)..."
-    7z a -aoa -y -r -stl -sdel -sse -ssp -ssw -ssc -bt -m0=LZMA2 -p"${MARIADB_BACKUP_PASSWORD}" "$logicBackup/$currentDate-${logicalName}.7z" "$logicBackup/$currentDate-${logicalName}.sql"
+    mariadb-dump --all-tablespaces --opt --add-drop-database --default-character-set=utf8mb4 --flush-privileges --flush-logs --tz-utc --quote-names --single-transaction --insert-ignore --routines --triggers --databases simbiatr_simbiat --tables "$tablesOrder" 2>> "$logFile" 1>> "$logicBackup/$currentDate-${logicalName}.sql"
     echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] Backing up full database for $currentDate (physical)..."
-    mariadb-backup --backup --target-dir=$physBackup --kill-long-queries-timeout=300 --extended-validation --lock-ddl-per-table
+    mariadb-backup --backup --target-dir="$physBackup" --kill-long-queries-timeout=300 --extended-validation --lock-ddl-per-table
     echo "Owning for $currentDate..."
     chown mysql:mysql $physBackup -R
     echo "Preparing $currentDate..."
     echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] Preparing backup for $currentDate (physical)..."
     mariadb-backup --prepare --target-dir=$physBackup
+    echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] Zipping users for $currentDate...";
+    7z a -aoa -y -r -stl -sdel -sse -ssp -ssw -ssc -bt -m0=LZMA2 -p"${MARIADB_BACKUP_PASSWORD}" "$logicBackup/$currentDate-users.7z" "$logicBackup/$currentDate-users.sql";
+    echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] Zipping data for $currentDate (logical)..."
+    7z a -aoa -y -r -stl -sdel -sse -ssp -ssw -ssc -bt -m0=LZMA2 -p"${MARIADB_BACKUP_PASSWORD}" "$logicBackup/$currentDate-${logicalName}.7z" "$logicBackup/$currentDate-${logicalName}.sql"
     echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] Zipping backup for $currentDate (physical)..."
     7z a -aoa -y -r -stl -sdel -sse -ssp -ssw -ssc -bt -m0=LZMA2 -p"${MARIADB_BACKUP_PASSWORD}" "$logicBackup/$currentDate-physical.7z" $physBackup
     echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] Backup completed"
