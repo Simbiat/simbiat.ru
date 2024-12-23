a53612f156dc902d2b9c05af2addf60d8343600d
Dmitry Kustov <simbiat@outlook.com>
2024-12-23 11:45:53 +0200

    Minor adjustments and FrankenPHP update

--- config/mysql/cron/backup.sh
+++ config/mysql/cron/backup.sh
@@ -5,7 +5,7 @@ if [ "$WEB_SERVER_TEST" != "true" ]; then
   logFile=/usr/local/logs/backup-$currentDate.log
   physBackup=/usr/local/backups/physical
   logicBackup=/usr/local/backups
-  tablesOrder=$(cat /usr/local/build/DDL/00-recommended_table_order.txt)
+  tablesOrder=$(cat /usr/local/DDL/00-recommended_table_order.txt)
   logicalName="daily"
   if [ "$(date +%u)" -eq 1 ]; then
       logicalName="weekly"
@@ -24,7 +24,7 @@ if [ "$WEB_SERVER_TEST" != "true" ]; then
     echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] Zipping users for $currentDate...";
     7z a -aoa -y -r -stl -sdel -sse -ssp -ssw -ssc -bt -m0=LZMA2 -p"${MARIADB_BACKUP_PASSWORD}" "$logicBackup/$currentDate-users.7z" "$logicBackup/$currentDate-users.sql";
     echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] Backing up data for $currentDate (logical)...";
-    mariadb-dump --all-tablespaces --opt --add-drop-database --default-character-set=utf8mb4 --flush-privileges --flush-logs --tz-utc --quote-names --single-transaction --insert-ignore --routines --triggers --databases simbiatr_simbiat --tables "$tablesOrder" 2>> "$logFile" 1>> "$logicBackup/$currentDate-${logicalName}.sql"
+    mariadb-dump --all-tablespaces --opt --add-drop-database --default-character-set=utf8mb4 --flush-privileges --flush-logs --tz-utc --quote-names --single-transaction --insert-ignore --routines --triggers --databases simbiatr_simbiat --tables $tablesOrder 2>> "$logFile" 1>> "$logicBackup/$currentDate-${logicalName}.sql"
     echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] Zipping data for $currentDate (logical)..."
     7z a -aoa -y -r -stl -sdel -sse -ssp -ssw -ssc -bt -m0=LZMA2 -p"${MARIADB_BACKUP_PASSWORD}" "$logicBackup/$currentDate-${logicalName}.7z" "$logicBackup/$currentDate-${logicalName}.sql"
     echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] Backing up full database for $currentDate (physical)..."
