5d0eb933535b1241dc8d0a62465682213c3f3714
Dmitry Kustov <simbiat@outlook.com>
2025-03-25 20:19:22 +0200

    Drop the test key prefix, since no longer used

--- config/mysql/cron/ssl.sh
+++ config/mysql/cron/ssl.sh
@@ -5,28 +5,24 @@
 currentDate=$(date +%Y.%m.%d)
 logFile=/usr/local/logs/ssl-$currentDate.log
 keysDir=/usr/local/keys
-keyPrefix=
-if [ "$WEB_SERVER_TEST" != "false" ]; then
-  keyPrefix=test_
-fi
 
 #Generate CA
 echo Generating CA for $currentDate... >> $logFile 2>&1
-openssl req -newkey rsa:2048 -nodes -keyout $keysDir/${keyPrefix}ca.key -subj /CN=${MARIADB_TLS_SETTING_CA_NAME} -out $keysDir/${keyPrefix}ca.req >> $logFile 2>&1
-openssl rsa -in $keysDir/${keyPrefix}ca.key -out $keysDir/${keyPrefix}ca.key >> $logFile 2>&1
-openssl x509 -sha256 -days 90 -set_serial 1 -req -in $keysDir/${keyPrefix}ca.req -signkey $keysDir/${keyPrefix}ca.key -out $keysDir/${keyPrefix}ca.crt >> $logFile 2>&1
+openssl req -newkey rsa:2048 -nodes -keyout $keysDir/ca.key -subj /CN=${MARIADB_TLS_SETTING_CA_NAME} -out $keysDir/ca.req >> $logFile 2>&1
+openssl rsa -in $keysDir/ca.key -out $keysDir/ca.key >> $logFile 2>&1
+openssl x509 -sha256 -days 90 -set_serial 1 -req -in $keysDir/ca.req -signkey $keysDir/ca.key -out $keysDir/ca.crt >> $logFile 2>&1
 
 #Generate server certificate
 echo Generating Server for $currentDate... >> $logFile 2>&1
-openssl req -newkey rsa:2048 -nodes -keyout $keysDir/${keyPrefix}server.key -subj /CN=${MARIADB_TLS_SETTING_SERVER_NAME} -addext "subjectAltName=${MARIADB_TLS_SETTING_SERVER_ALIAS}" -out $keysDir/${keyPrefix}server.req >> $logFile 2>&1
-openssl rsa -in $keysDir/${keyPrefix}server.key -out $keysDir/${keyPrefix}server.key >> $logFile 2>&1
-openssl x509 -sha256 -days 90 -set_serial 2 -req -in $keysDir/${keyPrefix}server.req -CA $keysDir/${keyPrefix}ca.crt -CAkey $keysDir/${keyPrefix}ca.key -out $keysDir/${keyPrefix}server.crt >> $logFile 2>&1
+openssl req -newkey rsa:2048 -nodes -keyout $keysDir/server.key -subj /CN=${MARIADB_TLS_SETTING_SERVER_NAME} -addext "subjectAltName=${MARIADB_TLS_SETTING_SERVER_ALIAS}" -out $keysDir/server.req >> $logFile 2>&1
+openssl rsa -in $keysDir/server.key -out $keysDir/server.key >> $logFile 2>&1
+openssl x509 -sha256 -days 90 -set_serial 2 -req -in $keysDir/server.req -CA $keysDir/ca.crt -CAkey $keysDir/ca.key -out $keysDir/server.crt >> $logFile 2>&1
 
 #Generate client certificate
 echo Generating Client for $currentDate... >> $logFile 2>&1
-openssl req -newkey rsa:2048 -nodes -keyout $keysDir/${keyPrefix}client.key -subj /CN=${MARIADB_TLS_SETTING_CLIENT_NAME} -out $keysDir/${keyPrefix}client.req >> $logFile 2>&1
-openssl rsa -in $keysDir/${keyPrefix}client.key -out $keysDir/${keyPrefix}client.key >> $logFile 2>&1
-openssl x509 -sha256 -days 90 -set_serial 3 -req -in $keysDir/${keyPrefix}client.req -CA $keysDir/${keyPrefix}ca.crt -CAkey $keysDir/${keyPrefix}ca.key -out $keysDir/${keyPrefix}client.crt >> $logFile 2>&1
+openssl req -newkey rsa:2048 -nodes -keyout $keysDir/client.key -subj /CN=${MARIADB_TLS_SETTING_CLIENT_NAME} -out $keysDir/client.req >> $logFile 2>&1
+openssl rsa -in $keysDir/client.key -out $keysDir/client.key >> $logFile 2>&1
+openssl x509 -sha256 -days 90 -set_serial 3 -req -in $keysDir/client.req -CA $keysDir/ca.crt -CAkey $keysDir/ca.key -out $keysDir/client.crt >> $logFile 2>&1
 
 #Own the files (otherwise MariaDB can fail to read private keys)
 echo Owning for $currentDate... >> $logFile 2>&1
