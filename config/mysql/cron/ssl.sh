99d63082bb1b8ab47c43301b27b1db730e35e8cb
Dmitry Kustov <simbiat@outlook.com>
2025-03-29 18:43:42 +0200

    Proper use of SAN

--- config/mysql/cron/ssl.sh
+++ config/mysql/cron/ssl.sh
@@ -6,23 +6,26 @@ currentDate=$(date +%Y.%m.%d)
 logFile=/usr/local/logs/ssl-$currentDate.log
 keysDir=/usr/local/keys
 
+#Generate SAN
+echo "subjectAltName=${MARIADB_TLS_SETTING_SERVER_ALIAS}" > $keysDir/san.cnf
+
 #Generate CA
 echo Generating CA for $currentDate... >> $logFile 2>&1
-openssl req -newkey rsa:2048 -nodes -keyout $keysDir/ca.key -subj /CN=${MARIADB_TLS_SETTING_CA_NAME} -out $keysDir/ca.req >> $logFile 2>&1
+openssl req -newkey rsa:2048 -nodes -keyout $keysDir/ca.key -subj /CN=${MARIADB_TLS_SETTING_CA_NAME} -addext "subjectAltName=${MARIADB_TLS_SETTING_SERVER_ALIAS}" -out $keysDir/ca.req >> $logFile 2>&1
 openssl rsa -in $keysDir/ca.key -out $keysDir/ca.key >> $logFile 2>&1
-openssl x509 -sha256 -days 90 -set_serial 1 -req -in $keysDir/ca.req -signkey $keysDir/ca.key -out $keysDir/ca.crt >> $logFile 2>&1
+openssl x509 -sha256 -days 90 -set_serial 1 -req -in $keysDir/ca.req -signkey $keysDir/ca.key -extfile $keysDir/san.cnf -out $keysDir/ca.crt >> $logFile 2>&1
 
 #Generate server certificate
 echo Generating Server for $currentDate... >> $logFile 2>&1
 openssl req -newkey rsa:2048 -nodes -keyout $keysDir/server.key -subj /CN=${MARIADB_TLS_SETTING_SERVER_NAME} -addext "subjectAltName=${MARIADB_TLS_SETTING_SERVER_ALIAS}" -out $keysDir/server.req >> $logFile 2>&1
 openssl rsa -in $keysDir/server.key -out $keysDir/server.key >> $logFile 2>&1
-openssl x509 -sha256 -days 90 -set_serial 2 -req -in $keysDir/server.req -CA $keysDir/ca.crt -CAkey $keysDir/ca.key -out $keysDir/server.crt >> $logFile 2>&1
+openssl x509 -sha256 -days 90 -set_serial 2 -req -in $keysDir/server.req -CA $keysDir/ca.crt -CAkey $keysDir/ca.key -extfile $keysDir/san.cnf -out $keysDir/server.crt >> $logFile 2>&1
 
 #Generate client certificate
 echo Generating Client for $currentDate... >> $logFile 2>&1
-openssl req -newkey rsa:2048 -nodes -keyout $keysDir/client.key -subj /CN=${MARIADB_TLS_SETTING_CLIENT_NAME} -out $keysDir/client.req >> $logFile 2>&1
+openssl req -newkey rsa:2048 -nodes -keyout $keysDir/client.key -subj /CN=${MARIADB_TLS_SETTING_CLIENT_NAME} -addext "subjectAltName=${MARIADB_TLS_SETTING_SERVER_ALIAS}" -out $keysDir/client.req >> $logFile 2>&1
 openssl rsa -in $keysDir/client.key -out $keysDir/client.key >> $logFile 2>&1
-openssl x509 -sha256 -days 90 -set_serial 3 -req -in $keysDir/client.req -CA $keysDir/ca.crt -CAkey $keysDir/ca.key -out $keysDir/client.crt >> $logFile 2>&1
+openssl x509 -sha256 -days 90 -set_serial 3 -req -in $keysDir/client.req -CA $keysDir/ca.crt -CAkey $keysDir/ca.key -extfile $keysDir/san.cnf -out $keysDir/client.crt >> $logFile 2>&1
 
 #Own the files (otherwise MariaDB can fail to read private keys)
 echo Owning for $currentDate... >> $logFile 2>&1
