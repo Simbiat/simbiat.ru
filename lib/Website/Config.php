9f1aa5797cdce6e0c687d05eb31cdc032732529e
Dmitrii Kustov <github@simbiat.dev>
2025-05-10 16:44:02 +0300

    Start building the installer function
    Signed-off-by: Dmitrii Kustov <github@simbiat.dev>

--- lib/Website/Config.php
+++ lib/Website/Config.php
@@ -7,9 +7,18 @@ namespace Simbiat\Website;
 
 #Database settings
 use Dotenv\Dotenv;
+use Simbiat\Cron\Agent;
+use Simbiat\Cron\Installer;
+use Simbiat\Cron\Task;
+use Simbiat\Cron\TaskInstance;
 use Simbiat\Database\Connection;
 use Simbiat\Database\Query;
 use Simbiat\Database\Pool;
+use Simbiat\Website\Cron\Maintenance;
+use Simbiat\Website\Cron\BICTracker;
+use Simbiat\Website\Cron\Talks;
+use Simbiat\Website\Cron\FFTracker;
+use Simbiat\Website\Sitemap\Generate;
 
 /**
  * Class that holds the main settings for the website. Needs to be instantiated early as part of bootstrapping.
@@ -227,5 +236,471 @@ final class Config
             }
         }
         return true;
+    }
+    
+    /**
+     * Logic for the initial installation of the website.
+     *
+     * @return void
+     */
+    private function install(): void
+    {
+        #Install CRON
+        if (new Installer()->install()) {
+            #Update settings
+            $agent = new Agent();
+            $agent->setSetting('logLife', 14);
+            $agent->setSetting('maxThreads', 10);
+            #Add tasks
+            new Task()->settingsFromArray([
+                'task' => 'argon',
+                'function' => 'argon',
+                'object' => '\\'.Maintenance::class,
+                'maxTime' => 3600,
+                'minFrequency' => 60,
+                'retry' => 0,
+                'enabled' => 1,
+                'system' => 1,
+                'description' => 'Job to recalculate optimal Argon encryption settings',
+            ])->add();
+            new Task()->settingsFromArray([
+                'task' => 'bicUpdate',
+                'function' => 'LibraryUpdate',
+                'object' => '\\'.BICTracker::class,
+                'maxTime' => 3600,
+                'minFrequency' => 60,
+                'retry' => 0,
+                'enabled' => 1,
+                'system' => 1,
+                'description' => 'Job to update BIC library',
+            ])->add();
+            new Task()->settingsFromArray([
+                'task' => 'cleanAvatars',
+                'function' => 'cleanAvatars',
+                'object' => '\\'.Talks::class,
+                'maxTime' => 3600,
+                'minFrequency' => 60,
+                'retry' => 0,
+                'enabled' => 1,
+                'system' => 1,
+                'description' => 'Removing excessive avatars',
+            ])->add();
+            new Task()->settingsFromArray([
+                'task' => 'cleanUploads',
+                'function' => 'cleanFiles',
+                'object' => '\\'.Talks::class,
+                'maxTime' => 3600,
+                'minFrequency' => 60,
+                'retry' => 0,
+                'enabled' => 1,
+                'system' => 1,
+                'description' => 'Removing unused and orphaned uploaded files',
+            ])->add();
+            new Task()->settingsFromArray([
+                'task' => 'cookiesClean',
+                'function' => 'cookiesClean',
+                'object' => '\\'.Maintenance::class,
+                'maxTime' => 3600,
+                'minFrequency' => 60,
+                'retry' => 0,
+                'enabled' => 1,
+                'system' => 1,
+                'description' => 'Job to purge old cookies',
+            ])->add();
+            new Task()->settingsFromArray([
+                'task' => 'dbForBackup',
+                'function' => 'forBackup',
+                'object' => '\\'.Maintenance::class,
+                'maxTime' => 3600,
+                'minFrequency' => 60,
+                'retry' => 0,
+                'enabled' => 1,
+                'system' => 1,
+                'description' => 'Generate DDLs and recommended dump order for current DB structure',
+            ])->add();
+            new Task()->settingsFromArray([
+                'task' => 'dbMaintenance',
+                'function' => 'dbOptimize',
+                'object' => '\\'.Maintenance::class,
+                'maxTime' => 3600,
+                'minFrequency' => 60,
+                'retry' => 0,
+                'enabled' => 1,
+                'system' => 1,
+                'description' => 'Job to optimize tables',
+            ])->add();
+            new Task()->settingsFromArray([
+                'task' => 'ffAddServers',
+                'function' => 'UpdateServers',
+                'object' => '\\'.FFTracker::class,
+                'maxTime' => 3600,
+                'minFrequency' => 60,
+                'retry' => 0,
+                'enabled' => 1,
+                'system' => 1,
+                'description' => 'Adds new servers',
+            ])->add();
+            new Task()->settingsFromArray([
+                'task' => 'ffNewCharacters',
+                'function' => 'registerNewCharacters',
+                'object' => '\\'.FFTracker::class,
+                'maxTime' => 3600,
+                'minFrequency' => 60,
+                'retry' => 0,
+                'enabled' => 1,
+                'system' => 1,
+                'description' => 'Schedule jobs to add potential new characters',
+            ])->add();
+            new Task()->settingsFromArray([
+                'task' => 'ffNewLinkshells',
+                'function' => 'registerNewLinkshells',
+                'object' => '\\'.FFTracker::class,
+                'maxTime' => 3600,
+                'minFrequency' => 60,
+                'retry' => 0,
+                'enabled' => 1,
+                'system' => 1,
+                'description' => 'Check for potential new linkshells and schedule jobs for them',
+            ])->add();
+            new Task()->settingsFromArray([
+                'task' => 'ffUpdateEntity',
+                'function' => 'UpdateEntity',
+                'object' => '\\'.FFTracker::class,
+                'allowedreturns' => '["character", "freecompany", "linkshell", "crossworldlinkshell", "pvpteam", "achievement"]',
+                'maxTime' => 3600,
+                'minFrequency' => 60,
+                'retry' => 0,
+                'enabled' => 1,
+                'system' => 1,
+                'description' => 'Update FFXIV entities',
+            ])->add();
+            new Task()->settingsFromArray([
+                'task' => 'ffUpdateOld',
+                'function' => 'UpdateOld',
+                'object' => '\\'.FFTracker::class,
+                'maxTime' => 3600,
+                'minFrequency' => 60,
+                'retry' => 0,
+                'enabled' => 1,
+                'system' => 1,
+                'description' => 'Update oldest FFXIV entities',
+            ])->add();
+            new Task()->settingsFromArray([
+                'task' => 'ffUpdateStatistics',
+                'function' => 'UpdateStatistics',
+                'object' => '\\'.FFTracker::class,
+                'maxTime' => 3600,
+                'minFrequency' => 60,
+                'retry' => 0,
+                'enabled' => 1,
+                'system' => 1,
+                'description' => 'Update FFXIV statistics',
+            ])->add();
+            new Task()->settingsFromArray([
+                'task' => 'filesClean',
+                'function' => 'filesClean',
+                'object' => '\\'.Maintenance::class,
+                'maxTime' => 3600,
+                'minFrequency' => 60,
+                'retry' => 0,
+                'enabled' => 1,
+                'system' => 1,
+                'description' => 'Delete old files',
+            ])->add();
+            new Task()->settingsFromArray([
+                'task' => 'lockPosts',
+                'function' => 'lockPosts',
+                'object' => '\\'.Talks::class,
+                'maxTime' => 3600,
+                'minFrequency' => 60,
+                'retry' => 0,
+                'enabled' => 1,
+                'system' => 1,
+                'description' => 'Locking posts for editing',
+            ])->add();
+            new Task()->settingsFromArray([
+                'task' => 'logsClean',
+                'function' => 'logsClean',
+                'object' => '\\'.Maintenance::class,
+                'maxTime' => 3600,
+                'minFrequency' => 60,
+                'retry' => 0,
+                'enabled' => 1,
+                'system' => 1,
+                'description' => 'Job to purge old logs',
+            ])->add();
+            new Task()->settingsFromArray([
+                'task' => 'sessionClean',
+                'function' => 'sessionClean',
+                'object' => '\\'.Maintenance::class,
+                'maxTime' => 3600,
+                'minFrequency' => 60,
+                'retry' => 0,
+                'enabled' => 1,
+                'system' => 1,
+                'description' => 'Job to purge old sessions',
+            ])->add();
+            new Task()->settingsFromArray([
+                'task' => 'sitemap',
+                'function' => 'generate',
+                'object' => '\\'.Generate::class,
+                'maxTime' => 3600,
+                'minFrequency' => 60,
+                'retry' => 0,
+                'enabled' => 1,
+                'system' => 1,
+                'description' => 'Job to generate text and XML sitemap files',
+            ])->add();
+            new Task()->settingsFromArray([
+                'task' => 'statisticsClean',
+                'function' => 'statisticsClean',
+                'object' => '\\'.Maintenance::class,
+                'maxTime' => 3600,
+                'minFrequency' => 60,
+                'retry' => 0,
+                'enabled' => 1,
+                'system' => 1,
+                'description' => 'Delete old statistical data',
+            ])->add();
+            #Adding task instances
+            new TaskInstance()->settingsFromArray([
+                'task' => 'argon',
+                'instance' => 1,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 2592000,
+                'priority' => 0,
+                'message' => 'Recalculating Argon settings',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'bicUpdate',
+                'instance' => 1,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 86400,
+                'priority' => 5,
+                'message' => 'Updating BIC library',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'cleanAvatars',
+                'instance' => 1,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 86400,
+                'priority' => 0,
+                'message' => 'Removing excessive avatars',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'cleanUploads',
+                'instance' => 1,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 86400,
+                'priority' => 0,
+                'message' => 'Cleaning uploaded files',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'cookiesClean',
+                'instance' => 1,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 60,
+                'priority' => 9,
+                'message' => 'Removing old cookies',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'dbForBackup',
+                'instance' => 1,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 86400,
+                'priority' => 9,
+                'message' => 'Dumping DDLs',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'dbMaintenance',
+                'instance' => 1,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 2592000,
+                'priority' => 0,
+                'message' => 'Running database maintenance',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'ffAddServers',
+                'instance' => 1,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 604800,
+                'dayofweek' => '[3]',
+                'priority' => 1,
+                'message' => 'Checking for new servers on Lodestone',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'ffNewCharacters',
+                'instance' => 1,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 86400,
+                'priority' => 1,
+                'message' => 'Scheduling potential new character',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'ffNewLinkshells',
+                'instance' => 1,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 3600,
+                'priority' => 1,
+                'message' => 'Checking for new linkshells',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'ffUpdateOld',
+                'arguments' => '[50, "$cronInstance"]',
+                'instance' => 1,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 60,
+                'priority' => 0,
+                'message' => 'Updating old FFXIV entities',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'ffUpdateOld',
+                'arguments' => '[50, "$cronInstance"]',
+                'instance' => 2,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 60,
+                'priority' => 0,
+                'message' => 'Updating old FFXIV entities',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'ffUpdateOld',
+                'arguments' => '[50, "$cronInstance"]',
+                'instance' => 3,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 60,
+                'priority' => 0,
+                'message' => 'Updating old FFXIV entities',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'ffUpdateOld',
+                'arguments' => '"[50, ""$cronInstance""]"',
+                'instance' => 4,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 60,
+                'priority' => 0,
+                'message' => 'Updating old FFXIV entities',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'ffUpdateOld',
+                'arguments' => '[50, "$cronInstance"]',
+                'instance' => 5,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 60,
+                'priority' => 0,
+                'message' => 'Updating old FFXIV entities',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'ffUpdateOld',
+                'arguments' => '[50, "$cronInstance"]',
+                'instance' => 6,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 60,
+                'priority' => 0,
+                'message' => 'Updating old FFXIV entities',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'ffUpdateStatistics',
+                'instance' => 1,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 86400,
+                'priority' => 2,
+                'message' => 'Updating FFXIV statistics',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'filesClean',
+                'instance' => 1,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 604800,
+                'priority' => 0,
+                'message' => 'Removing old files',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'lockPosts',
+                'instance' => 1,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 3600,
+                'priority' => 9,
+                'message' => 'Locking posts',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'logsClean',
+                'instance' => 1,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 2592000,
+                'priority' => 9,
+                'message' => 'Removing old logs',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'sessionClean',
+                'instance' => 1,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 60,
+                'priority' => 9,
+                'message' => 'Removing old sessions',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'sitemap',
+                'instance' => 1,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 86400,
+                'priority' => 0,
+                'message' => 'Generating sitemap files',
+                'nextrun' => '',
+            ])->add();
+            new TaskInstance()->settingsFromArray([
+                'task' => 'statisticsClean',
+                'instance' => 1,
+                'enabled' => 1,
+                'system' => 1,
+                'frequency' => 2592000,
+                'priority' => 0,
+                'message' => 'Removing old statistical data',
+                'nextrun' => '',
+            ])->add();
+            
+        }
     }
 }
