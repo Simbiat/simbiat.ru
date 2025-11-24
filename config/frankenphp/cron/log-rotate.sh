66adecb49c2f8b18befc4663a7851e0fc27be39e
Dmitrii Kustov <github@simbiat.eu>
2025-11-24 07:17:41 +0200

    Remove php log renaming
    Signed-off-by: Dmitrii Kustov <github@simbiat.eu>

--- config/frankenphp/cron/log-rotate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -f /app/logs/php.log ]; then
-    # Copy the log file to the new file with the previous day's date
-    mv -f --no-copy -T /app/logs/php.log "/app/logs/php-$(date -d "yesterday" +%Y.%m.%d).log"
-fi
-exit
