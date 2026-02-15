e0be51485b8d95ad485dce0fde0f5dcf2e558a9e
Dmitrii Kustov <github@simbiat.eu>
2026-02-15 18:49:36 +0200

    Achievement points and mutual relationships
    Signed-off-by: Dmitrii Kustov <github@simbiat.eu>

--- build/DDL/080-ffxiv__freecompany_character.sql
+++ build/DDL/080-ffxiv__freecompany_character.sql
@@ -7,7 +7,6 @@ CREATE TABLE IF NOT EXISTS `ffxiv__freecompany_character` (
   PRIMARY KEY (`character_id`,`fc_id`),
   KEY `fc_xchar_fc` (`fc_id`),
   KEY `fc_char_rank` (`rank_id`),
-  CONSTRAINT `fc_char_rank` FOREIGN KEY (`rank_id`) REFERENCES `ffxiv__freecompany_rank` (`rank_id`) ON DELETE SET NULL ON UPDATE SET NULL,
   CONSTRAINT `fc_xchar_fc` FOREIGN KEY (`fc_id`) REFERENCES `ffxiv__freecompany` (`fc_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fc_xchar_id` FOREIGN KEY (`character_id`) REFERENCES `ffxiv__character` (`character_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_as_cs ROW_FORMAT=DYNAMIC COMMENT='Characters linked to Free Companies, past and present' `PAGE_COMPRESSED`='ON';
