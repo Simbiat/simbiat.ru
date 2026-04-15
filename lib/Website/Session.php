727f6240a96c6f8bb4f261f4da6c5a2eb0515363
Dmitrii Kustov <github@simbiat.eu>
2026-04-15 15:05:59 +0300

    Adjusted handling of deadlocks for GC
    Signed-off-by: Dmitrii Kustov <github@simbiat.eu>

--- lib/Website/Session.php
+++ lib/Website/Session.php
@@ -434,10 +434,7 @@ final class Session implements \SessionHandlerInterface, \SessionIdInterface, \S
     public function gc(int $max_lifetime = 300): false|int
     {
         try {
-            Query::$max_tries = 1;
-            $result = Query::query('DELETE FROM `uc__sessions` WHERE `time` <= DATE_SUB(CURRENT_TIMESTAMP(6), INTERVAL :life SECOND) OR `user_id` IN (:system_user_id, :deleted_user_id);', [':life' => [$max_lifetime, 'int'], ':system_user_id' => [SystemUsers::System->value, 'int'], ':deleted_user_id' => [SystemUsers::Deleted->value, 'int']], return: 'affected');
-            Query::$max_tries = 5;
-            return $result;
+            return Query::query('DELETE FROM `uc__sessions` WHERE `time` <= DATE_SUB(CURRENT_TIMESTAMP(6), INTERVAL :life SECOND) OR `user_id` IN (:system_user_id, :deleted_user_id);', [':life' => [$max_lifetime, 'int'], ':system_user_id' => [SystemUsers::System->value, 'int'], ':deleted_user_id' => [SystemUsers::Deleted->value, 'int']], return: 'affected');
         } catch (\Throwable $throwable) {
             #Ignore deadlocks
             if (mb_stripos($throwable->getMessage(), 'Deadlock', 0, 'UTF-8') === false) {
